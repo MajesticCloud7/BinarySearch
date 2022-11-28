5504895ffc8c36a6b036ac4f15f943490dfdee6b
Rugilė <rugile4@gmail.com>
2022-11-28 23:32:36 +0200

    Refactor BinarySearchExample

--- src/BinarySearchExample.java
+++ src/BinarySearchExample.java
@@ -2,54 +2,55 @@
  * Written by: Chaitanya from beginnersbook.com
  * Input: Number of elements, element's values, value to be searched
  * Output:Position of the number input by user among other numbers*/
+
 import java.util.Scanner;
-class BinarySearchExample
-{
-    public static void main(String args[])
-    {
-        byte num, item, array[], first, last, middle;
+
+class BinarySearchExample {
+    public static void main(String args[]) {
+        int num, item, array[];
         //To capture user input
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of elements:");
-        num = input.nextByte();
+        num = input.nextInt();
 
         //Creating array to store the all the numbers
-        array = getIntArray(num, input, 0);
+        array = getIntArray(num, input);
 
-        //Asking user to enter values
         System.out.println("Enter the search value:");
-        item = input.nextByte();
-        first = 0;
-        last = (byte) (num - 1);
-        middle = (byte) ((first + last)/2);
+        item = input.nextInt();
 
-        while( first <= last )
-        {
-            if ( array[middle] < item )
-                first = (byte) (middle + 1);
-            else if ( array[middle] == item )
-            {
-                System.out.println(item + " found at location " + (middle + 1) + ".");
-                break;
-            }
-            else
-            {
-                last = (byte) (middle - 1);
-            }
-            middle = (byte) ((first + last)/2);
-        }
-        if ( first > last )
+        if (!search(num, array, item))
             System.out.println(item + " is not found.\n");
     }
 
-    private static byte[] getIntArray(int num, Scanner input, int cntr) {
-        byte array[] = new byte[num];
+    private static int[] getIntArray(int num, Scanner input) {
+        int[] array = new int[num];
 
         System.out.println("Enter " + num + " integers");
         //Loop to store each numbers in array
-        for (cntr = 0; cntr < num; cntr++)
-            array[cntr] = input.nextByte();
+        for (int i = 0; i < num; i++)
+            array[i] = input.nextInt();
 
         return array;
+    }
+
+    private static boolean search(int num, int[] array, int item) {
+        int first = 0;
+        int last = (byte) (num - 1);
+        int middle = (byte) ((first + last) / 2);
+
+        while (first <= last) {
+            if (array[middle] < item)
+                first = (byte) (middle + 1);
+            else if (array[middle] == item) {
+                System.out.println(item + " found at location " + (middle + 1) + ".");
+                break;
+            } else {
+                last = (byte) (middle - 1);
+            }
+            middle = (byte) ((first + last) / 2);
+        }
+
+        return first <= last;
     }
 }
